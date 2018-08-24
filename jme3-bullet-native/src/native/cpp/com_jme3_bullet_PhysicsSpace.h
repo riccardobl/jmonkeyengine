71b1b5b62b8b4c6deb36b91b852db6b7f7ddac84
Riccardo Balbo <riccardo@forkforge.net>
2018-08-24 17:12:04 +0200

    Commit generated header

--- jme3-bullet-native/src/native/cpp/com_jme3_bullet_PhysicsSpace.h
+++ jme3-bullet-native/src/native/cpp/com_jme3_bullet_PhysicsSpace.h
@@ -16,10 +16,10 @@ extern "C" {
 /*
  * Class:     com_jme3_bullet_PhysicsSpace
  * Method:    createPhysicsSpace
- * Signature: (FFFFFFIZ)J
+ * Signature: (FFFFFFII)J
  */
 JNIEXPORT jlong JNICALL Java_com_jme3_bullet_PhysicsSpace_createPhysicsSpace
-  (JNIEnv *, jobject, jfloat, jfloat, jfloat, jfloat, jfloat, jfloat, jint, jboolean);
+  (JNIEnv *, jobject, jfloat, jfloat, jfloat, jfloat, jfloat, jfloat, jint, jint);
 
 /*
  * Class:     com_jme3_bullet_PhysicsSpace
