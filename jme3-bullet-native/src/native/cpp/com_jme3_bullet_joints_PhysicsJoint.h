b3ac0b4c36cd6b4fefec29d2a152cee39269cd3c
Riccardo Balbo <riccardo@forkforge.net>
2018-11-28 16:54:48 +0100

    Regen JNI

--- jme3-bullet-native/src/native/cpp/com_jme3_bullet_joints_PhysicsJoint.h
+++ jme3-bullet-native/src/native/cpp/com_jme3_bullet_joints_PhysicsJoint.h
@@ -18,7 +18,7 @@ JNIEXPORT jfloat JNICALL Java_com_jme3_bullet_joints_PhysicsJoint_getAppliedImpu
 /*
  * Class:     com_jme3_bullet_joints_PhysicsJoint
  * Method:    setBreakingImpulseThreshold
- * Signature: (JF)F
+ * Signature: (JF)V
  */
 JNIEXPORT void JNICALL Java_com_jme3_bullet_joints_PhysicsJoint_setBreakingImpulseThreshold
   (JNIEnv *, jobject, jlong, jfloat);
